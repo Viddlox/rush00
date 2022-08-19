5426c87b0bd553321145f04b151ecfcfc6168e19
Goh Hin Chik Daryl <gdaryl@u91z05s03.42kl.edu.my>
2022-08-19 16:25:46 +0800

    Fixed putchr

---
+++ rush03.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush03.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gdaryl <gdaryl@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/19 15:45:43 by gdaryl            #+#    #+#             */
+/*   Updated: 2022/08/19 16:24:41 by gdaryl           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int		ft_putchar(char c);
+
+void	ft_print_manager(int x, char start, char mid, char end)
+{
+	ft_putchar(start);
+	while ((x - 1) > 1)
+	{
+		ft_putchar(mid);
+		x--;
+	}
+	if (x > 1)
+		ft_putchar(end);
+	ft_putchar('\n');
+}
+
+void	rush(int x, int y)
+{
+	if (x > 0 && y > 0)
+	{
+		ft_print_manager(x, 'A', 'B', 'C');
+		y--;
+		while (y > 1)
+		{
+			ft_print_manager(x, 'B', ' ', 'B');
+			y--;
+		}
+		if (y > 0)
+			ft_print_manager(x, 'A', 'B', 'C');
+	}
+	return ;
+}
