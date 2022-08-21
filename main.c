5be74bb4d3dc6d1ef7ba3f674c8f11523a6e8e3d
Hong You Wong <hwong@u82z05s01.42kl.edu.my>
2022-08-21 18:30:12 +0800

    final commit rush00

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hotan <hotan@student.42kl.edu.my>          +#+  +:+       +#+        */
+/*   By: hwong <hwong@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/08/19 15:58:52 by gdaryl            #+#    #+#             */
-/*   Updated: 2022/08/21 17:44:41 by hotan            ###   ########.fr       */
+/*   Created: 2022/08/21 17:51:23 by ysar@studen       #+#    #+#             */
+/*   Updated: 2022/08/21 18:18:51 by hwong            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,55 @@
 
 void	rush(int x, int y);
 
-int	main(void)
+int	ft_integer_only(char *str)
 {
-	rush(5, 3);
-	return (0);
+	int	output;
+	int	base;
+	int	deducted;
+
+	deducted = 0;
+	base = 1;
+	while (*str != '\0')
+	{
+		if (*str < 48 || *str > 57)
+			return (-1);
+		str++;
+		deducted++;
+		base *= 10;
+	}
+	base /= 10;
+	str -= deducted;
+	output = 0;
+	while (*str != '\0')
+	{
+		output += (*str - 48) * base;
+		base /= 10;
+		str++;
+	}
+	return (output);
+}
+
+int	main(int argc, char *argv[])
+{
+	int	i_col;
+	int	i_row;
+
+	i_col = 5;
+	i_row = 5;
+	if (argc == 1)
+		rush(i_col, i_row);
+	else if (argc == 3)
+	{
+		i_col = ft_integer_only(argv[1]);
+		i_row = ft_integer_only(argv[2]);
+		if (i_col == -1 || i_row == -1)
+			write(1, "Please insert: column row\n", 28);
+		else
+		{
+			rush(i_col, i_row);
+			return (0);
+		}
+	}
+	else
+		write(1, "Please insert: column row\n", 28);
 }
